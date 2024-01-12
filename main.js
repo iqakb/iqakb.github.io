8afbbcb9451b4c03afa424440b96e6d7a30c640d
akbar <achmadiqbalakbari@gmail.com>
2024-01-13 01:22:46 +0700

    feat: add axis

--- main.js
+++ main.js
@@ -10,6 +10,8 @@ document.body.appendChild( renderer.domElement );
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh( geometry, material );
+const axesHelper = new THREE.AxesHelper( 5 );
+scene.add( axesHelper );
 scene.add( cube );
 
 camera.position.z = 5;
