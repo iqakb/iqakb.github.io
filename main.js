ac446bbab1812d05259081ecd267e332f443f5e7
akbar <achmadiqbalakbari@gmail.com>
2024-01-13 01:31:10 +0700

    feat: add orbit controls

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -6,6 +7,8 @@ const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.inner
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
+const controls = new OrbitControls( camera, renderer.domElement );
+
 
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
@@ -15,6 +18,8 @@ scene.add( axesHelper );
 scene.add( cube );
 
 camera.position.z = 5;
+controls.update();
+
 
 function animate() {
 	requestAnimationFrame( animate );
@@ -22,6 +27,7 @@ function animate() {
 	cube.rotation.x += 0.01;
 	cube.rotation.y += 0.01;
 
+    controls.update();
 	renderer.render( scene, camera );
 }
 
