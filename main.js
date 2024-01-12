053418dfe34033b0b5e202781ca84654dc765637
akbar <achmadiqbalakbari@gmail.com>
2024-01-13 02:08:06 +0700

    feat: adding light to the scene

--- main.js
+++ main.js
@@ -8,6 +8,10 @@ const color3 = new THREE.Color(0xffc000);
 scene.background = color3;
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
+const light = new THREE.AmbientLight( 0x404040 ); // soft white light
+scene.add( light );
+
+
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
